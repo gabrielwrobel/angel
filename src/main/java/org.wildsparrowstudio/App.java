7fd460823130f1236a9273e5815697f9fcd026ea
bobrys <6abriel.wrobel@gmail.com>
2025-10-05 13:06:28 +0200

    init commit

---
+++ src/main/java/org.wildsparrowstudio/App.java
@@ -0,0 +1,14 @@
+package org.wildsparrowstudio;
+
+public class App
+{
+    public  int numer = 4;
+    public static void main( String[] args )
+    {
+        System.out.println("lala" );
+    }
+     int returnNum(){
+        return numer;
+    }
+}
+
